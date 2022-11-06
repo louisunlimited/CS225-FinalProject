b4f37712f3ef0f4a85fd0857803d857d0379a370
cty012 <tianyue.cao21@gmail.com>
2022-11-06 17:35:49 -0600

    ...

--- src/filereader.cpp
+++ src/filereader.cpp
@@ -1,1 +1,26 @@
 #include "filereader.h"
+
+// Qian Yu
+vector<Coord> FileReader::readNormalizedNode(string fileName) {
+    vector<Coord> nodes;
+    return nodes;
+}
+
+vector<Coord> FileReader::convertNode(vector<Coord> normalizedCoords,
+    Coord anchor1, Coord normalizedAnchor1,
+    Coord anchor2, Coord normalizedAnchor2) {
+
+    vector<Coord> nodes;
+    return nodes;
+}
+
+// Jiang Hezi
+vector<tuple<int, int>> FileReader::readEdge(string fileName) {
+    vector<tuple<int, int>> edges;
+    return edges;
+}
+
+vector<Coord> FileReader::readPoliceStation(string fileName) {
+    vector<Coord> police;
+    return police;
+}
