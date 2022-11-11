84d35f66e3c9dca1f4374a4101d68dfe9b8da5dd
Louis Qian <yuq4@illinois.edu>
2022-11-10 23:31:20 -0600

    added inital unit test for FileReader

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,31 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Test 1", "[test1]") {
-   REQUIRE(1 == 1);
+#include "filereader.h"
+
+TEST_CASE("Test FileReader readEdge", "[FileReader]") {
+    FileReader fr;
+    vector<std::pair<int, int>> edges = fr.readEdge("../data/SF.cedge.txt");
+
+    // check edge read
+    REQUIRE(edges.size() == 223001);
+    REQUIRE(edges[0].first == 0);
+    REQUIRE(edges[0].second == 5);
+    REQUIRE(edges[258].first == 5905);
+    REQUIRE(edges[258].second == 5908);
+    REQUIRE(edges[132812].first == 111607);
+    REQUIRE(edges[132812].second == 111618);
+    REQUIRE(edges[223000].first == 743);
+    REQUIRE(edges[223000].second == 745);
+}
+
+TEST_CASE("Test FileReader readPoliceStation", "[FileReader]") {
+    FileReader fr;
+    vector<Coord> police = fr.readPoliceStation("../data/Map_of_Police_Stations__2011_.csv");
+
+    // check police station read
+    REQUIRE(police.size() == 10);
+    REQUIRE(police[0].lat_ == 37.7986453192461);
+    REQUIRE(police[0].long_ == -122.4098629092911);
+    REQUIRE(police[9].lat_ == 37.7801611403781);
+    REQUIRE(police[9].long_ == -122.432390435179);
 }
