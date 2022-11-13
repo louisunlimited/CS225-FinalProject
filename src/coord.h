54966e710bb4880b79f9d4270f62c91668a55add
Louis Qian <yuq4@illinois.edu>
2022-11-13 16:58:00 -0600

    Revert "resolve linker error"
    This reverts commit 8f715ace3c6186b30903ae2d34b66b9d049cde45.

--- src/coord.h
+++ src/coord.h
@@ -29,7 +29,13 @@ struct Coord {
  * @param y The second coordinate
  * @return The distance in km
  */
-double dist(Coord x, Coord y);
+double dist(Coord x, Coord y) {
+    double phi1 = x.lat_ / 180.0 * M_PI;
+    double phi2 = y.lat_ / 180.0 * M_PI;
+    double thetaDiff = (x.long_ - y.long_) / 180.0 * M_PI;
+    double cosAlp = sin(phi1) * sin(phi2) * cos(thetaDiff) + cos(phi1) * cos(phi2);
+    return acos(cosAlp) * RADIUS;
+}
 
 /**
  * Find the distance between two normalized coordinates
@@ -38,4 +44,8 @@ double dist(Coord x, Coord y);
  * @param y The second coordinate
  * @return The normalized distance
  */
-double normalizedDist(Coord x, Coord y);
+double normalizedDist(Coord x, Coord y) {
+    double latDiff = x.lat_ - y.lat_;
+    double longDiff = x.long_ - y.long_;
+    return sqrt(latDiff * latDiff + longDiff * longDiff);
+}
