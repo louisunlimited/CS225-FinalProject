65e8ac383083735bc6900af9b7a24ef2e3f71def
Louis Qian <yuq4@illinois.edu>
2022-11-15 15:50:40 -0600

    initial testcase for readNode

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,8 +2,19 @@
 
 #include "filereader.h"
 
-TEST_CASE("readNormalizedNode", "[weight=1][part=1]") {
-    vector<Coord> nodes = FileReader::readNormalizedNode("../data/SF.cnode.txt");
+vector<Coord> makeCoords() {
+    vector<Coord> coords;
+    for (int i = 0; i < 10; i++) {
+        Coord coord;
+        coord.long_ = i;
+        coord.lat_ = i;
+        coords.push_back(coord);
+    }
+    return coords;
+}
+
+TEST_CASE("readRawNode", "[FileReader]") {
+    vector<Coord> nodes = FileReader::readRawNode("../data/SF.cnode.txt");
     REQUIRE(nodes.size() == 174956);
     REQUIRE(nodes[0].long_ == 1905.934692);
     REQUIRE(nodes[0].lat_ == 2760.598145);
@@ -13,4 +24,29 @@ TEST_CASE("readNormalizedNode", "[weight=1][part=1]") {
     REQUIRE(nodes[91899].lat_ == 8581.037109);
     REQUIRE(nodes[174955].long_ == 3999.741943);
     REQUIRE(nodes[174955].lat_ == 5436.662598);
+}
+
+TEST_CASE("convertNode", "[FileReader]") {
+    vector<Coord> nodes = makeCoords(); // coords from 0 to 9
+
+    Coord anchor1, anchor2;
+    anchor1.long_ = 1;
+    anchor1.lat_ = 1;
+    anchor2.long_ = 2;
+    anchor2.lat_ = 2;
+    Coord normalizedAnchor1, normalizedAnchor2;
+    normalizedAnchor1.long_ = 2;
+    normalizedAnchor1.lat_ = 2;
+    normalizedAnchor2.long_ = 8;
+    normalizedAnchor2.lat_ = 8;
+
+    FileReader::convertNode(nodes, anchor1, normalizedAnchor1, anchor2, normalizedAnchor2);
+    REQUIRE(nodes.size() == 10); 
+    REQUIRE(nodes[2].long_ == 1);
+    REQUIRE(nodes[2].lat_ == 1);
+    REQUIRE(nodes[8].long_ == 2);
+    REQUIRE(nodes[8].lat_ == 2);
+    REQUIRE(nodes[9].long_ > 2.1);
+    REQUIRE(nodes[9].lat_ > 2.1);
+
 }
